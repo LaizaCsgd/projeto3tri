50415f3e0dfe77554672d570e1a6d2c1c7979413
LaizaCsgd <32268645+LaizaCsgd@users.noreply.github.com>
2017-10-23 12:04:14 -0200

    trabalho 23/10

--- Trab.3tri/detalhaProfessor.php
+++ Trab.3tri/detalhaProfessor.php
@@ -9,23 +9,48 @@
 
 	//chama função, passando o codigo do professor
 	$professor = buscaProfessor($siape);
+
 ?>
-
-	<div>
-		<img src="<?=$professor['foto']?>">
-		<h2><?=$professor['nome']?></h2>
-		<p><?=$professor['email']?></p>
-		
-	</div>
-
 	
 
+	<!-- colunas para centralizar -->
+	<div class="coluna10">.</div>
 
-
+	<!-- conteudo principal -->
+	<article class="coluna80">
 
 <?php
 
+	
+	include("professores.php");
+	//a função retorna um array contendo os professores
+	$lista = buscaProfessor($siape);
 
+	
+
+		print('
+
+			<section class="foto">
+				<img src="imagens/pessoa.png">
+			</section>
+
+			<section class="dados">
+				<p>Nome: '.$lista["nome"].'</p>
+				<p>Email: '.$lista["email"].'</p>
+				<p>Disciplinas: Fundamento de Informática</p>
+				<p>Sala: B7</p>
+
+			</section>
+	
+
+
+			');
+	
+
+?>
+
+
+<?php
 
 	include("rodape.php")
 
